792a9a5e0a32650da48c71f7eff54d78aa15405f
heidi505 <xxwhite19@gmail.com>
2023-11-03 16:02:06 +0900

    관리자 페이지 로그인 완료, 다른 기능 진행중

--- src/main/java/com/example/kakao/admin/AdminController.java
+++ src/main/java/com/example/kakao/admin/AdminController.java
@@ -1,9 +1,13 @@
 package com.example.kakao.admin;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.h2.engine.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.kakao._core.utils.ApiUtils;
+import com.example.kakao.admin.AdminResponse.adminLoginResponseDTO;
+import com.example.kakao.user.User;
 import com.example.kakao.user.UserRequest;
 import com.example.kakao.user.UserResponse;
 
@@ -20,17 +26,32 @@ import com.example.kakao.user.UserResponse;
 @RequiredArgsConstructor
 @Controller
 public class AdminController {
+
+    @Autowired
+    private final AdminService adminService;
+    private final HttpSession session;
     
     @GetMapping("/admin")
     public String admin() {
        
-        return "admin";
+        return "admin/admin";
     }
 
     @PostMapping("/admin/login")
-    public String login() {
-        UserResponse.loginResponseDTO responseDTO = userService.login(requestDTO);
-        return ResponseEntity.ok().header("Authorization", "Bearer " + responseDTO.getJwt()).body(ApiUtils.success(responseDTO));
+    public String adminLogin(AdminRequest.AdminLoginRequestDTO adminLoginRequestDTO, Model model) {
+        adminLoginResponseDTO adminLoginResponseDTO = adminService.adminLogin(adminLoginRequestDTO);
+
+        
+        User user = (User) session.getAttribute("sessionUser");
+        model.addAttribute("sessionUser", user);
+
+        return "admin/adminWebtoon";
     }
+
+    @GetMapping("/admin/adminAuthorManaging")
+    public String adminAuthorManaging() {
+        return "admin/adminAuthorManaging";
+    }
+    
     
 }
--- src/main/java/com/example/kakao/admin/AdminResponse.java
+++ src/main/java/com/example/kakao/admin/AdminResponse.java
@@ -1,5 +1,21 @@
 package com.example.kakao.admin;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 public class AdminResponse {
+
+    @Getter
+    @Setter
+    @ToString
+    public static class adminLoginResponseDTO {
+        private String jwt;
+
+        public adminLoginResponseDTO(String jwt) {
+            this.jwt = jwt;
+        }
+        
+    }
     
 }
