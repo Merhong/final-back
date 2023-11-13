46becfab9ca472bd10e11f092dc020552a403dad
k7850 <k79357850@gmail.com>
2023-11-13 15:44:10 +0900

    관리자웹

--- src/test/java/com/example/kakao/webtoon/WebtoonControllerTest.java
+++ src/test/java/com/example/kakao/webtoon/WebtoonControllerTest.java
@@ -25,13 +25,14 @@ public class WebtoonControllerTest extends MyWithRestDoc {
     // @Autowired
     // private MockMvc mvc;
 
-    @Test
-    public void findAll_test() throws Exception {
-        // given
-        String jwt="Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtZXRhY29kaW5nLWtleSIsImlkIjozLCJlbWFpbCI6ImFkbWluQG5hdmVyLmNvbSIsImNvb2tpZSI6MCwidXNlcm5hbWUiOiLqtIDrpqzsnpAiLCJ1c2VyVHlwZUVudW0iOiJBRE1JTiIsImV4cCI6MTcwMDQ0ODc1M30.kBxBp7O8wG_s0xQb_p-myFaMKuuNRBVWWXaKAIpmsoUTX5wt1agnvlflS9pQ1hGBhgjGNpWPqw3ACHh-5BAR1Q";
+    String jwt="Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtZXRhY29kaW5nLWtleSIsImlkIjozLCJlbWFpbCI6ImFkbWluQG5hdmVyLmNvbSIsImNvb2tpZSI6MCwidXNlcm5hbWUiOiLqtIDrpqzsnpAiLCJ1c2VyVHlwZUVudW0iOiJBRE1JTiIsImV4cCI6MTcwMDQ0ODc1M30.kBxBp7O8wG_s0xQb_p-myFaMKuuNRBVWWXaKAIpmsoUTX5wt1agnvlflS9pQ1hGBhgjGNpWPqw3ACHh-5BAR1Q";
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", jwt);
+    
+    @Test
+    public void find_all_test() throws Exception {
+        // given
+        // HttpHeaders headers = new HttpHeaders();
+        // headers.add("Authorization", jwt);
         
         // when
         ResultActions resultActions = mockMvcAddFilter.perform(
@@ -60,24 +61,16 @@ public class WebtoonControllerTest extends MyWithRestDoc {
 
 
     @Test
-    public void login_test() throws Exception {
+    public void find_by_id_test() throws Exception {
         // given
-        UserRequest.LoginDTO requestDTO =
-                new UserRequest.LoginDTO();
-        requestDTO.setEmail("ssar@naver.com");
-        requestDTO.setPassword("123455555555555");
-        ObjectMapper om = new ObjectMapper();
-        String requestBody = om.writeValueAsString(requestDTO);
-        System.out.println("================================");
-        System.out.println(requestBody);
-        System.out.println("================================");
+       
+        int webtoonId = 101;
 
         // when
-        ResultActions resultActions = mockMvc.perform(
+        ResultActions resultActions = mockMvcAddFilter.perform(
                 MockMvcRequestBuilders
-                        .post("/login")
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .get("/webtoons/"+webtoonId)
+                        .header(HttpHeaders.AUTHORIZATION, jwt)
         );
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println(responseBody);
@@ -86,11 +79,12 @@ public class WebtoonControllerTest extends MyWithRestDoc {
         resultActions
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value("ssar@naver.com"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value("쌀"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.userTypeEnum").value("NORMAL"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.id").value(101))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.title").value("외모지상주의"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.errorType").isEmpty())
                 .andDo(MockMvcResultHandlers.print())
                 .andDo(document);
     }
+
+
 }
