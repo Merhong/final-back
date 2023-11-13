48bb5a79aacd4da409ce2ab181943527c7ba644c
k7850 <k79357850@gmail.com>
2023-11-13 10:16:47 +0900

    mysql적용

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -22,13 +22,13 @@ VALUES (7, 300, '쿠키 300개', 36000);
 
 
 INSERT INTO user_tb (`id`, `email`, `password`, `user_type_enum`, `username`)
-VALUES ('1', 'ssar@naver.com', '1234', 'NORMAL', '쌀');
+VALUES ('1', 'ssar@naver.com', '123455555555555', 'NORMAL', '쌀');
 INSERT INTO user_tb (`id`, `email`, `password`, `user_type_enum`, `username`)
-VALUES ('2', 'cos@naver.com', '1234', 'AUTHOR', '코스');
+VALUES ('2', 'cos@naver.com', '12345555555555', 'AUTHOR', '코스');
 INSERT INTO user_tb (`id`, `email`, `password`, `user_type_enum`, `username`)
 VALUES ('3', 'admin@naver.com', '1234', 'ADMIN', '관리자');
 INSERT INTO user_tb (`id`, `email`, `password`, `user_type_enum`, `username`)
-VALUES ('4', 'love@naver.com', '1234', 'NORMAL', '럽브');
+VALUES ('4', 'love@naver.com', '123455555555', 'NORMAL', '럽브');
 
 
 -------------------------------------------------------------------------------------------------------------------------
