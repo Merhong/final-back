3f37456997701cd4505a665e1cb66877eee149f0
k7850 <k79357850@gmail.com>
2023-11-13 09:09:27 +0900

    푸시알림백1

--- src/main/java/com/example/kakao/KakaoApplication.java
+++ src/main/java/com/example/kakao/KakaoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KakaoApplication {
-    // 231107 fix
+    
     public static void main(String[] args) {
         SpringApplication.run(KakaoApplication.class, args);
     }
